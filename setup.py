e9bf8e8318525768d4a9340526cae84f87544c6e
Masaya Kataoka <ms.kataoka@gmail.com>
2021-10-12 22:16:16 +0900

    update version
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/hakuturu583/mkdocs_github_dashboard',
-    version='0.1.0',
+    version='0.2.0',
     zip_safe=False,
     entry_points={
         'mkdocs.plugins': [
