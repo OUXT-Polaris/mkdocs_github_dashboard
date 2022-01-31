da1e2e81a3c9ea4dd123bbeef1f75383104d3e77
MasayaKataoka <ms.kataoka@gmail.com>
2022-01-31 23:17:23 +0900

    add debug print
    Signed-off-by: MasayaKataoka <ms.kataoka@gmail.com>

--- mkdocs_github_dashboard/mkdocs_github_dashboard.py
+++ mkdocs_github_dashboard/mkdocs_github_dashboard.py
@@ -18,6 +18,12 @@ class MkDocsGithubDashboardPlugin(BasePlugin):
                     if workflow["state"] == "active":
                         dict[package_url][workflow["name"]] = "![Not Found](" + workflow["badge_url"] + ")"
             badge_urls = pd.DataFrame().from_dict(dict)
+            print(badge_urls.T)
             markdown = markdown.replace(replace_target, badge_urls.T.to_markdown())
 
         return markdown
+
+if __name__ == '__main__':
+    plugin = MkDocsGithubDashboardPlugin()
+    text = "@github_dashboard(OUXT-Polaris/color_names,OUXT-Polaris/data_buffer,OUXT-Polaris/dynamixel_hardware_interface,OUXT-Polaris/geographic_conversion,OUXT-Polaris/geographic_info,OUXT-Polaris/geometry_msgs_data_buffer,OUXT-Polaris/hermite_path_planner,OUXT-Polaris/image_processing_utils,OUXT-Polaris/joy_to_twist,OUXT-Polaris/lua_vendor,OUXT-Polaris/message_synchronizer,OUXT-Polaris/miniv_control,OUXT-Polaris/miniv_description,OUXT-Polaris/navi_sim,OUXT-Polaris/nmea_gps_driver,OUXT-Polaris/nmea_hardware_interface,OUXT-Polaris/nmea_to_geopose,OUXT-Polaris/odom_frame_publisher,OUXT-Polaris/ouxt_common,OUXT-Polaris/pcl_apps,OUXT-Polaris/perception_bringup,OUXT-Polaris/playstation_controller_drivers,OUXT-Polaris/quaternion_operation,OUXT-Polaris/realsense_hardware_interface,OUXT-Polaris/robotx_behavior_tree,OUXT-Polaris/robotx_costmap_calculator,OUXT-Polaris/robotx_ekf,OUXT-Polaris/scan_segmentation,OUXT-Polaris/sol_vendor,OUXT-Polaris/tcp_sender,OUXT-Polaris/unisim_ros2_control,OUXT-Polaris/usv_controller)"
+    plugin.on_page_markdown(text)
