3ba19cc6b17daa8e4a04b162921a1af5014a180a
MasayaKataoka <ms.kataoka@gmail.com>
2022-03-29 00:40:31 +0900

    add pull request table
    Signed-off-by: MasayaKataoka <ms.kataoka@gmail.com>

--- mkdocs_github_dashboard/mkdocs_github_dashboard.py
+++ mkdocs_github_dashboard/mkdocs_github_dashboard.py
@@ -9,24 +9,32 @@ class MkDocsGithubDashboardPlugin(BasePlugin):
         github = Github()
         for m in re.finditer(r'@github_dashboard\(.+?\)', markdown, re.MULTILINE):
             replace_target = m.group()
-            print(replace_target)
+            #print(replace_target)
             packages = replace_target[18:len(replace_target)-1].split(',')
-            dict = {}
+            workflow_dict = {}
+            pull_request_dict = {}
             for package in packages:
                 package_url = "[" + package + "](https://github.com/" + package + ")"
-                dict[package_url] = {}
+                workflow_dict[package_url] = {}
+                pull_request_dict[package_url] = {}
                 print("processing package : " + package)
                 if "workflows" in github.workflow.get(package).keys():
                     for workflow in github.workflow.get(package)["workflows"]:
                         if workflow["state"] == "active" and workflow["name"] != "pages-build-deployment":
-                            dict[package_url][workflow["name"]] = "![Not Found](" + workflow["badge_url"] + ")"
-            badge_urls = pd.DataFrame().from_dict(dict)
-            markdown = markdown.replace(replace_target, badge_urls.T.to_markdown())
+                            workflow_dict[package_url][workflow["name"]] = "![Not Found](" + workflow["badge_url"] + ")"
+                for pull_request in github.pull_request.get(package):
+                    text = "[" + pull_request["title"] + "](" + pull_request["html_url"] + ")"
+                    pull_request_dict[package_url]["pull_request"] = text
+            badge_urls = pd.DataFrame().from_dict(workflow_dict)
+            pull_requests = pd.DataFrame().from_dict(pull_request_dict)
+            #print(pull_requests.T.to_markdown())
+            markdown = markdown.replace(replace_target, badge_urls.T.to_markdown() + "\n\n" + pull_requests.T.to_markdown())
         return markdown
 
 if __name__ == '__main__':
     plugin = MkDocsGithubDashboardPlugin()
-    text = "@github_dashboard(OUXT-Polaris/color_names,OUXT-Polaris/data_buffer,OUXT-Polaris/dynamixel_hardware_interface,OUXT-Polaris/geographic_conversion,OUXT-Polaris/geographic_info)"
+    #text = "@github_dashboard(OUXT-Polaris/color_names,OUXT-Polaris/data_buffer,OUXT-Polaris/dynamixel_hardware_interface,OUXT-Polaris/geographic_conversion,OUXT-Polaris/geographic_info)"
+    text = "@github_dashboard(OUXT-Polaris/dynamixel_hardware_interface)"
     markdown = plugin.on_page_markdown(text)
     print("")
     print(markdown)
